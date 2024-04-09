10d27feaf3fefd976905d9489dfa908d78306022
unknown <tranthitrinh4013@gmail.com>
2024-04-09 14:27:09 +0700

    update profile page

--- page/profilePage/profile.js
+++ page/profilePage/profile.js
@@ -65,93 +65,193 @@ function confirmLogout(event) {
         window.location.href = "../login-logout/logout.html";
     }
 }
-function toggleEditSave() {
+
+function setPhoneNew() {
+    var regexPhone = /^(03|05|07|08|09|01[2|6|8|9])([0-9]{8})\b$/;
+    var phoneInput = document.getElementById("phoneInput").value;
     var phoneValue = document.getElementById("phoneValue");
-    var currentPhone = phoneValue.textContent.trim();
-    var editButton = event.target;
+    var editButton = document.getElementById("editButton");
+    var editContainer = document.getElementById("editContainer");
+    var errPhone = document.getElementById("errPhone");
 
-    if (editButton.textContent === "Chỉnh sửa") {
-        var input = document.createElement("input");
-        input.type = "text";
-        input.value = currentPhone;
-        phoneValue.textContent = "";
-        phoneValue.appendChild(input);
-
-        editButton.textContent = "Lưu";
-    } else {
-        var input = phoneValue.querySelector("input");
-        var newPhone = input.value;
-        phoneValue.removeChild(input);
-        phoneValue.textContent = newPhone;
-        editButton.textContent = "Chỉnh sửa";
-    }
-}
-function toggleBirth() {
-    var phoneValue = document.getElementById("birthValue");
-    var currentPhone = phoneValue.textContent.trim();
-    var editButton = event.target;
-
-    if (editButton.textContent === "Chỉnh sửa") {
-        var input = document.createElement("input");
-        input.type = "text";
-        input.value = currentPhone;
-        phoneValue.textContent = "";
-        phoneValue.appendChild(input);
-
-        editButton.textContent = "Lưu";
-    } else {
-        var input = phoneValue.querySelector("input");
-        var newDate = input.value;
-        phoneValue.removeChild(input);
-        phoneValue.textContent = newDate;
-        editButton.textContent = "Chỉnh sửa";
-    }
-}
-
-function toggleAddpre() {
-    var phoneValue = document.getElementById("addpreValue");
-    var editButton = event.target;
-
-    if (editButton.textContent === "Chỉnh sửa") {
-        var input = document.createElement("input");
-        input.type = "text";
-        input.value = phoneValue.textContent.trim(); // Không cần dùng currentPhone, có thể truy cập trực tiếp thuộc tính textContent
-        phoneValue.textContent = "";
-        phoneValue.appendChild(input);
-
-        editButton.textContent = "Lưu";
-    } else {
-        var input = phoneValue.querySelector("input");
-
-        // Kiểm tra xem input có tồn tại không trước khi truy cập thuộc tính value
-        var newAddpress = input ? input.value : "";
-
-        if (input) {
-            phoneValue.removeChild(input);
-            phoneValue.textContent = newAddpress;
+    if (phoneValue.style.display === "none") {
+        // Nếu trường nhập liệu đang hiển thị, kiểm tra và lưu giá trị mới
+        if (regexPhone.test(phoneInput)) {
+            phoneValue.innerText = phoneInput;
+            phoneValue.style.display = "inline";
+            editContainer.style.display = "none";
             editButton.textContent = "Chỉnh sửa";
+            errPhone.innerText = "*";
+
+            // Lưu giá trị vào localStorage
+            localStorage.setItem("phoneValue", phoneInput);
+        } else {
+            errPhone.innerText = "Số điện thoại không đúng định dạng!";
+        }
+    } else {
+        // Nếu trường nhập liệu đang ẩn, chuyển sang chế độ chỉnh sửa
+        phoneValue.style.display = "none";
+        editContainer.style.display = "inline";
+        editButton.textContent = "Lưu";
+        document.getElementById("phoneInput").value = phoneValue.textContent; // Khởi tạo giá trị của trường nhập liệu từ <p>
+    }
+}
+function setBirthNew() {
+    var regexBirth =
+        /^(((0[1-9]|[12][0-9]|3[01])([/])(0[13578]|10|12)([/])(\d{4}))|(([0][1-9]|[12][0-9]|30)([/])(0[469]|11)([/])(\d{4}))|((0[1-9]|1[0-9]|2[0-8])([/])(02)([/])(\d{4}))|((29)(\/)(02)([/])([02468][048]00))|((29)([/])(02)([/])([13579][26]00))|((29)([/])(02)([/])([0-9][0-9][0][48]))|((29)([/])(02)([/])([0-9][0-9][2468][048]))|((29)([/])(02)([/])([0-9][0-9][13579][26])))$/;
+    var birthInput = document.getElementById("birthInput").value;
+    var birthValue = document.getElementById("birthValue");
+    var editBirthButton = document.getElementById("editBirthButton");
+    var editBirthContainer = document.getElementById("editBirthContainer");
+    var errBirth = document.getElementById("errBirth");
+
+    if (birthValue.style.display === "none") {
+        if (regexBirth.test(birthInput)) {
+            birthValue.innerText = birthInput;
+            birthValue.style.display = "inline";
+            editBirthContainer.style.display = "none";
+            editBirthButton.textContent = "Chỉnh sửa";
+            errBirth.innerText = "*";
+
+            // Lưu giá trị vào localStorage
+            localStorage.setItem("birthValue", birthInput);
+        } else {
+            errBirth.innerText =
+                "Ngày sinh không đúng định dạng! VD:(01/01/2004)";
+        }
+    } else {
+        birthValue.style.display = "none";
+        editBirthContainer.style.display = "inline";
+        editBirthButton.textContent = "Lưu";
+        document.getElementById("birthInput").value = birthValue.textContent;
+    }
+}
+
+function setNatiNew() {
+    var regexNati = /^[^0-9]*$/;
+
+    var natiInput = document.getElementById("natiInput").value;
+    var natiValue = document.getElementById("natiValue");
+    var editNatiButton = document.getElementById("editNatiButton");
+    var editNatiContainer = document.getElementById("editNatiContainer");
+    var errNati = document.getElementById("errNati");
+
+    if (natiValue.style.display === "none") {
+        if (regexNati.test(natiInput)) {
+            natiValue.innerText = natiInput;
+            natiValue.style.display = "inline";
+            editNatiContainer.style.display = "none";
+            editNatiButton.textContent = "Chỉnh sửa";
+            errNati.innerText = "*";
+
+            // Lưu giá trị vào localStorage
+            localStorage.setItem("natiValue", natiInput);
+        } else {
+            errNati.innerText =
+                "Ngày sinh không đúng định dạng! VD:(01/01/2004)";
+        }
+    } else {
+        natiValue.style.display = "none";
+        editNatiContainer.style.display = "inline";
+        editNatiButton.textContent = "Lưu";
+        document.getElementById("natiInput").value = natiValue.textContent;
+    }
+}
+
+function setDiacNew() {
+    var regexDiac = /^.{1,40}$/;
+
+    var diacInput = document.getElementById("diacInput").value;
+    var diacValue = document.getElementById("diacValue");
+    var editDiacButton = document.getElementById("editDiacButton");
+    var editDiacContainer = document.getElementById("editDiacContainer");
+    var errDiac = document.getElementById("errDiac");
+
+    if (diacValue.style.display === "none") {
+        if (regexDiac.test(diacInput)) {
+            diacValue.innerText = diacInput;
+            diacValue.style.display = "inline";
+            editDiacContainer.style.display = "none";
+            editDiacButton.textContent = "Chỉnh sửa";
+            errDiac.innerText = "*";
+
+            // Lưu giá trị vào localStorage
+            localStorage.setItem("diacValue", diacInput);
+        } else {
+            errDiac.innerText = "Địa chỉ tối đa 30 ký tự";
+        }
+    } else {
+        diacValue.style.display = "none";
+        editDiacContainer.style.display = "inline";
+        editDiacButton.textContent = "Lưu";
+        document.getElementById("diacInput").value = diacValue.textContent;
+    }
+}
+
+//localStorage lưu thông tin người dùng
+window.onload = function () {
+    var savedBirth = localStorage.getItem("birthValue");
+    var savedPhone = localStorage.getItem("phoneValue");
+    var savedNati = localStorage.getItem("natiValue");
+    var savedDiac = localStorage.getItem("diacValue");
+    if (savedBirth) {
+        document.getElementById("birthValue").innerText = savedBirth;
+    }
+
+    if (savedPhone) {
+        document.getElementById("phoneValue").innerText = savedPhone;
+    }
+    if (savedNati) {
+        document.getElementById("natiValue").innerText = savedNati;
+    }
+    if (savedDiac) {
+        document.getElementById("diacValue").innerText = savedDiac;
+    }
+};
+
+//localStorage lưu select theo dõi
+document.addEventListener("DOMContentLoaded", function () {
+    var radioButtons = document.querySelectorAll('input[type="radio"]');
+    radioButtons.forEach(function (radioButton) {
+        radioButton.addEventListener("change", handleRadioChange);
+    });
+
+    var savedChoice = localStorage.getItem("userChoice");
+    if (savedChoice) {
+        var selectedRadioButton = document.getElementById(savedChoice);
+        if (selectedRadioButton) {
+            selectedRadioButton.checked = true;
         }
     }
+});
+
+function handleRadioChange(event) {
+    var selectedRadioButtonId = event.target.id;
+    localStorage.setItem("userChoice", selectedRadioButtonId);
 }
 
-function diaCAddpre() {
-    var phoneValue = document.getElementById("diacValue");
-    var currentPhone = phoneValue.textContent.trim();
-    var editButton = event.target;
-
-    if (editButton.textContent === "Chỉnh sửa") {
-        var input = document.createElement("input");
-        input.type = "text";
-        input.value = currentPhone;
-        phoneValue.textContent = "";
-        phoneValue.appendChild(input);
-
-        editButton.textContent = "Lưu";
-    } else {
-        var input = phoneValue.querySelector("input");
-        var newDiaChi = input.value;
-        phoneValue.removeChild(input);
-        phoneValue.textContent = newDiaChi;
-        editButton.textContent = "Chỉnh sửa";
+//localStorage lưu ưu đãi
+document.addEventListener("DOMContentLoaded", function () {
+    function saveToLocalStorage() {
+        var checkbox1 = document.getElementById("checkbox1");
+        var checkbox2 = document.getElementById("checkbox2");
+        localStorage.setItem("checkbox1", checkbox1.checked);
+        localStorage.setItem("checkbox2", checkbox2.checked);
     }
-}
+
+    function restoreFromLocalStorage() {
+        var checkbox1 = document.getElementById("checkbox1");
+        var checkbox2 = document.getElementById("checkbox2");
+        checkbox1.checked = localStorage.getItem("checkbox1") === "true";
+        checkbox2.checked = localStorage.getItem("checkbox2") === "true";
+    }
+
+    restoreFromLocalStorage();
+
+    document
+        .getElementById("checkbox1")
+        .addEventListener("change", saveToLocalStorage);
+    document
+        .getElementById("checkbox2")
+        .addEventListener("change", saveToLocalStorage);
+});
