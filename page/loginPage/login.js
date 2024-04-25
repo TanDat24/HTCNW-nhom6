da138d17f2ed4431c3c65f0095d66b6f0dab00b3
unknown <tranthitrinh4013@gmail.com>
2024-04-25 14:14:09 +0700

    pull login

---
+++ page/loginPage/login.js
@@ -0,0 +1,63 @@
+document.addEventListener("DOMContentLoaded", function () {
+    // Bắt sự kiện submit cho form đăng nhập bằng email
+    document
+        .querySelector("#EmailFormWrapper form")
+        .addEventListener("submit", function (event) {
+            event.preventDefault(); // Ngăn chặn việc gửi form mặc định
+
+            var email = document.getElementById("email").value;
+            var password = document.getElementById("pwd").value;
+
+            if (email === "nguyenvana@gmail.com" && password === "123456789") {
+                // Hiển thị overlay mờ và spinner
+                document.getElementById("overlay").style.display = "block";
+
+                // Đợi 1 giây trước khi chuyển hướng
+                setTimeout(function () {
+                    // Chuyển hướng người dùng đến trang homePage.html
+                    window.location.href = "../homePage/homePage.html";
+                }, 1000); // 1000 miliseconds = 1 giây
+            } else {
+                alert("Mật khẩu hoặc email không đúng. Vui lòng kiểm tra lại!");
+            }
+        });
+
+    // Bắt sự kiện submit cho form đăng nhập bằng số điện thoại
+    document
+        .querySelector("#PhoneFormWrapper form")
+        .addEventListener("submit", function (event) {
+            event.preventDefault(); // Ngăn chặn việc gửi form mặc định
+
+            var phone = document.getElementById("SDT").value;
+            var password = document.getElementById("pwd").value;
+
+            // Kiểm tra nếu số điện thoại và mật khẩu đúng
+            if (phone === "0912345678" && password === "123456789") {
+                // Hiển thị overlay mờ và spinner
+                document.getElementById("overlay").style.display = "block";
+
+                // Đợi 1 giây trước khi chuyển hướng
+                setTimeout(function () {
+                    // Chuyển hướng người dùng đến trang homePage.html
+                    window.location.href = "../homePage/homePage.html";
+                }, 1000); // 1000 miliseconds = 1 giây
+            } else {
+                alert(
+                    "Số điện thoại hoặc mật khẩu không đúng. Vui lòng kiểm tra lại!"
+                );
+            }
+        });
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+    // Lắng nghe sự kiện click trên nút đăng nhập
+    document
+        .querySelector(".custom-btn")
+        .addEventListener("click", function () {
+            // Lấy đối tượng ô email
+            var emailInput = document.getElementById("email");
+
+            // Focus vào ô email
+            emailInput.focus();
+        });
+});
